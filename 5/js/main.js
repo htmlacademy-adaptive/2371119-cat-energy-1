e4378041a5f3cda0db3aeb6bfeeb73168c5e7ad1
Keks <keks@htmlacademy.ru>
2023-11-16 09:19:34 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector(".navbar");
+const navbarButton = document.querySelector(".navbar__button");
+
+navbarButton.addEventListener("click", () => {
+  if (navbar.classList.contains("navbar--closed")) {
+    navbar.classList.remove("navbar--closed");
+  } else {
+    navbar.classList.add("navbar--closed");
+  }
+});
+
+function removeNoJs() {
+  document.querySelector(".page__body--nojs").classList.remove("page__body--nojs");
+  document.querySelector(".main-header--nojs").classList.remove("main-header--nojs");
+  document.querySelector(".main-header__logo--nojs").classList.remove("main-header__logo--nojs");
+  document.querySelector(".navbar--nojs").classList.remove("navbar--nojs");
+}
+
+removeNoJs();
