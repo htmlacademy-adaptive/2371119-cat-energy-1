2680dc93cabfed8601025d9cbb095488afdc4b42
LevLvov0820 <levlvov0820@gmail.com>
2023-12-04 17:37:23 +0300

    ошибки + pixel perfect

--- gulpfile.js
+++ gulpfile.js
@@ -2,157 +2,165 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
 import csso from 'postcss-csso';
 import rename from 'gulp-rename';
-import autoprefixer from 'autoprefixer';
-import htmlmin from 'gulp-htmlmin';
 import terser from 'gulp-terser';
-import imagemin from 'gulp-imagemin';
-import webp from 'gulp-webp';
+import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
-import { deleteAsync } from 'del';
+import del from 'del';
 import browser from 'browser-sync';
 
 // Styles
 
-export const styles = (done) => {
-  gulp.src('source/sass/style.scss', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer(),
-      csso()
-    ]))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
-    .pipe(browser.stream());
-  done();
+export const styles = () => {
+return gulp.src('source/sass/style.scss', { sourcemaps: true })
+.pipe(plumber())
+.pipe(sass().on('error', sass.logError))
+.pipe(postcss([
+autoprefixer(),
+csso()
+]))
+.pipe(rename('style.min.css'))
+.pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+.pipe(browser.stream());
 }
 
 // HTML
 
-export const html = () => {
-  return gulp.src('source/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build'));
+const html = () => {
+return gulp.src('source/*.html')
+.pipe(gulp.dest('build'));
 }
 
 // Scripts
 
 const scripts = () => {
-  return gulp.src('source/js/*.js')
-    .pipe(terser())
-    .pipe(gulp.dest('build/js'))
+return gulp.src('source/js/*.js')
+.pipe(gulp.dest('build/js'))
+.pipe(browser.stream());
 }
 
 // Images
 
-export const optimizeImages = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
-    .pipe(imagemin())
-    .pipe(gulp.dest('build/img'))
+const optimizeImages = () => {
+return gulp.src('source/img/**/*.{png,jpg}')
+.pipe(squoosh())
+.pipe(gulp.dest('build/img'))
 }
 
 const copyImages = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
-    .pipe(gulp.dest('build/img'))
+return gulp.src('source/img/**/*.{png,jpg}')
+.pipe(gulp.dest('build/img'))
 }
 
 // WebP
 
 const createWebp = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
-    .pipe(webp())
-    .pipe(gulp.dest('build/img'))
+return gulp.src('source/img/**/*.{png,jpg}')
+.pipe(squoosh({
+webp: {}
+}))
+.pipe(gulp.dest('build/img'))
 }
 
 // SVG
 
 const svg = () =>
-  gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
-    .pipe(svgo())
-    .pipe(gulp.dest('build/img'));
-
+gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+.pipe(svgo())
+.pipe(gulp.dest('build/img'));
 
 const sprite = () => {
-  return gulp.src('source/img/icons/*.svg')
-    .pipe(svgo())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'))
+return gulp.src('source/img/icons/*.svg')
+.pipe(svgo())
+.pipe(svgstore({
+inlineSvg: true
+}))
+.pipe(rename('sprite.svg'))
+.pipe(gulp.dest('build/img'));
 }
 
-//Copy
+// Copy
 
-const copy = () => {
-  return gulp.src([
-    'source/fonts/**/*.{woff2,woff}',
-    'source/*ico',
-  ], {
-    base: 'source'
-  })
-    .pipe(gulp.dest('build'))
+const copy = (done) => {
+gulp.src([
+'source/fonts/**/*.{woff2,woff}',
+'source/*.ico',
+], {
+base: 'source'
+})
+.pipe(gulp.dest('build'))
+done();
 }
 
 // Clean
 
 const clean = () => {
-  return deleteAsync('build');
-}
+return del('build');
+};
 
 // Server
 
 const server = (done) => {
-  browser.init({
-    server: {
-      baseDir: 'build'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
+browser.init({
+server: {
+baseDir: 'build'
+},
+cors: true,
+notify: false,
+ui: false,
+});
+done();
+}
+
+// Reload
+
+const reload = (done) => {
+browser.reload();
+done();
 }
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html', gulp.series(html));
+gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+gulp.watch('source/js/*.js', gulp.series(scripts));
+gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 // Build
 
 export const build = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-    svg,
-    sprite
-  ),
+clean,
+copy,
+optimizeImages,
+gulp.parallel(
+styles,
+html,
+scripts,
+svg,
+sprite,
+createWebp
+),
 );
 
+// Default
 
 export default gulp.series(
-  clean,
-  copy,
-  copyImages,
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-    svg,
-    sprite,
-    createWebp
-  ),
-  gulp.series(
-    server,
-    watcher
-  ));
+clean,
+copy,
+copyImages,
+gulp.parallel(
+styles,
+html,
+scripts,
+svg,
+sprite,
+createWebp
+),
+gulp.series(
+server,
+watcher
+));
--- source/js/main.js
+++ source/js/main.js
@@ -1,82 +1,9 @@
 const navbar = document.querySelector(".navbar");
 const navbarButton = document.querySelector(".navbar__button");
-const slider = document.querySelector(".slider");
-const sliderBefore = slider.querySelector(".slider__before");
-const beforeImage = sliderBefore.querySelector("img");
-const sliderThumb = slider.querySelector(".slider__thumb");
-const body = document.body;
 
 let isActive = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  let width = slider.offsetWidth;
-  beforeImage.style.width = `${width}px`;
-});
-
-const beforeAfterSlider = (x) => {
-  let shift = Math.max(0, Math.min(x, slider.offsetWidth));
-  sliderBefore.style.width = `${shift}px`;
-  sliderThumb.style.left = `${shift}px`;
-}
-
-const pauseEvenets = (e) => {
-  e.stopPropagation();
-  e.preventDefault();
-  return false;
-};
-
-body.addEventListener('mousedown', () => {
-  isActive = true;
-});
-
-body.addEventListener('mouseup', () => {
-  isActive = false;
-});
-
-body.addEventListener('mouseleave', () => {
-  isActive = false;
-});
-
-body.addEventListener('mousemove', (e) => {
-  if (!isActive) {
-    return;
-  }
-
-  let x = e.pageX;
-
-  x -= slider.getBoundingClientRect().left;
-  beforeAfterSlider(x);
-  pauseEvenets(e);
-});
-
-body.addEventListener('touchstart', () => {
-  isActive = true;
-});
-
-body.addEventListener('touchend', () => {
-  isActive = false;
-});
-
-body.addEventListener('touchcancel', () => {
-  isActive = false;
-});
-
-body.addEventListener('touchmove', () => {
-  if (!isActive) {
-    return;
-  }
-
-  let x;
-  let i;
-
-  for (i = 0; e < e.changedTouches.length; i++) {
-    x = e.changedTouches[i].pageX;
-  }
-
-  x -= slider.getBoundingClientRect().left;
-  beforeAfterSlider(x);
-  pauseEvenets(e);
-});
+removeNoJs();
 
 navbarButton.addEventListener("click", () => {
   if (navbar.classList.contains("navbar--closed")) {
@@ -92,5 +19,3 @@ function removeNoJs() {
   document.querySelector(".main-header__logo--nojs").classList.remove("main-header__logo--nojs");
   document.querySelector(".navbar--nojs").classList.remove("navbar--nojs");
 }
-
-removeNoJs();
